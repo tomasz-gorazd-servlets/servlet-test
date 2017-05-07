20d5abda2a93461fbab660fd667a48fafafcf5cd
ironbone <iron.bone@wp.pl>
2017-05-07 18:18:48 +0200

    maven ze starego

--- src/main/java/lu/lllc/ShowName1.java
+++ src/main/java/lu/lllc/ShowName1.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,7 +12,7 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class ShowName1
  */
-@WebServlet("/ShowName1")
+
 public class ShowName1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
